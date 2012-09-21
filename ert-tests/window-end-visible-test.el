8062790ddb99c824656b7ab19441417aabdfd34b
Roland Walker <walker@pobox.com>
2012-09-21 14:14:58 -0400

    include build and autoload with test target add stub test file no individual tests defined, though

---
+++ ert-tests/window-end-visible-test.el
@@ -0,0 +1,20 @@
+
+;; requires and setup
+
+(require 'window-end-visible)
+
+;;; this is a stub - no tests defined
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; window-end-visible-test.el ends here
